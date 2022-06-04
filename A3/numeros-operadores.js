15379402ce2830aa6600070972e652be3dd7c9b4
JoasGomes <jojoca.vitor777@gmail.com>
2022-06-04 09:58:38 -0300

    números e operadores

---
+++ A3/numeros-operadores.js
@@ -0,0 +1,30 @@
+var idade = 28;
+var gols = 1000;
+var pi = 3.14; //ponto para decimal
+var exp = 2e10; //20000000000
+
+//operadores
+var soma = 100 + 50;
+var subtracao = 100 - 50;
+var multiplicacao = 100 * 50;
+var divisao = 100 / 50;
+var expoente = 100 ** 50;
+var modulo = 100 % 50;
+
+//a ordem importa
+var total1 = 20 + 5 * 2; //30
+var total2 = (20 + 5) * 2; //50
+
+//incremento e decremento
+//incremento dps da primeira conjuração
+var x = 1;
+console.log(x++);
+console.log(x)
+
+//incrementa na primeira conjuração
+var y = 1
+console.log(++y);
+
+//drecremnto mesma coisa só que diminui
+var z = 1
+console.log(--z)
