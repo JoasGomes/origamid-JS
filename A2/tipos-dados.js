42210e5eb383c269587ff0ca0b60b6958a1fb0d4
JoasGomes <jojoca.vitor777@gmail.com>
2022-06-04 09:41:31 -0300

    tipos de dados

---
+++ A2/tipos-dados.js
@@ -0,0 +1,20 @@
+var nome = 'andre'; //string
+var sobrenome = 'gomes';
+var nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
+
+var idade = 28; //number
+var possuiFaculdade = true; //boolean
+var time; //undefined
+var comida = null; //null
+var simbolo = Symbol() //symbol
+var novoObjeto = {} //object
+
+console.log(typeof nome); // mostra o tipo de dado
+
+//template strings
+var gols = 1000;
+var frase = `romário fez ${gols} gols na copa`;
+console.log(frase);
+
