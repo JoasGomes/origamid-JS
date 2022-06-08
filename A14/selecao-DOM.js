9ea8028999f3dd093b12033997fb0d7cc2bcbb3b
JoasGomes <jojoca.vitor777@gmail.com>
2022-06-08 11:13:52 -0300

    seleção DOM

---
+++ A14/selecao-DOM.js
@@ -0,0 +1,31 @@
+//selecina pelo id
+const animaisSection = document.getElementById('animais');
+
+const contatoSection = document.getElementById('contato');
+
+//retorna null caso não exista
+const naoExiste = document.getElementById('pqpqpqp'); //null
+
+
+//seleciona pela classe, retorna HTMLCollection
+const gridSection = document.getElementsByClassName('grid-section');
+
+const contato = document.getElementsByClassName('grid-section contato');
+
+//seleciona todas as ul's e retorna HTMLCollection
+const ul = document.getElementsByTagName('ul');
+
+//seletor CSS
+const animais = document.querySelector('.animais');
+
+const contato2 = document.querySelector('#contato');
+
+const ultimoItem = document.querySelector('.animais-lista li:last-child');
+
+const linkCSS = document.querySelector('[href^="https://"]');
+
+const primeiroUl = document.querySelector('ul');
+
+//selecionar todos
+const animaisImg = document.querySelectorAll('.animais img');
+
