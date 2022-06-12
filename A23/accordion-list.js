f6e2e2b342089f29540f318dd0e33b9230065c84
JoasGomes <jojoca.vitor777@gmail.com>
2022-06-12 17:21:41 -0300

    accordion list

---
+++ A23/accordion-list.js
@@ -0,0 +1,40 @@
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+    if(tabMenu.length && tabContent.length) {
+        tabContent[0].classList.remove('ativo');
+
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+            tabContent[index].classList.add('ativo');
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            })
+        });
+    }
+}
+initTabNav();
+
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    if(accordionList.length) {
+        accordionList[0].classList.add('ativo');
+        accordionList[0].nextElementSibling.classList.add('ativo');
+
+        function activeAccordion() {
+            this.classList.add('ativo');
+            this.nextElementSibling.classList.add('ativo');
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        });
+    }
+}
+initAccordion();
