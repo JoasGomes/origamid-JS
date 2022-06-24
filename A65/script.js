a813b6c53185b2d7f63f47a35cda3a860653449e
JoasGomes <jojoca.vitor777@gmail.com>
2022-06-24 09:47:44 -0300

    validador de CPF

--- A65/script.js
+++ A65/script.js
@@ -1,6 +1,4 @@
 import ValidarCPF from "./validar-cpf.js";
 
 const cpf = document.querySelector('#cpf');
-const validarCpf = new ValidarCPF(cpf);
-
-console.log(validarCpf.fomatar('434-433.434.44'));
+const validarCpf = new ValidarCPF(cpf).iniciar();
--- A65/validar-cpf.js
+++ A65/validar-cpf.js
@@ -14,4 +14,41 @@ export default class ValidarCPF {
         const cpfLimpo = this.limpar(cpf);
         return this.construir(cpfLimpo);
     }
+
+    validar(cpf) {
+        const matchCpf = cpf.match(/(?:\d{3}[-.\s]?){3}\d{2}/g);
+        return (matchCpf && matchCpf[0] === cpf);
+    }
+
+    validarMudanca(cpfElement) {
+        if(this.validar(cpfElement.value)) {
+            cpfElement.value = this.fomatar(cpfElement.value);
+            cpfElement.classList.add('valido');
+            cpfElement.classList.remove('erro');
+            cpfElement.nextElementSibling.classList.remove('ativar');
+        } else {
+            cpfElement.classList.add('erro');
+            cpfElement.classList.remove('valido');
+            cpfElement.nextElementSibling.classList.add('ativar');
+        }
+    }
+
+    adicionarEvento() {
+        this.element.addEventListener('change', () => {
+            this.validarMudanca(this.element);
+        })
+    }
+
+    adicionarErroSpan() {
+        const erroElement = document.createElement('span');
+        erroElement.classList.add('erro-text');
+        erroElement.innerText = 'CPF Inválido';
+        this.element.parentElement.insertBefore(erroElement, this.element.nextElementSibling);
+    }
+
+    iniciar() {
+        this.adicionarEvento();
+        this.adicionarErroSpan();
+        return this;
+    }
 }
